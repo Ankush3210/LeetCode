bc5ad5d98540d45d624d24038e509a3f1d534f17
Ankush3210 <69753906+Ankush3210@users.noreply.github.com>
2026-05-25 14:14:47 +0530

    Create TwoSum.java

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,16 @@
+//Problem Link: https://leetcode.com/problems/two-sum
+// Approach: Solved using a nested loop brute force approach.
+
+class Solution {
+    public int[] twoSum(int[] nums, int target)
+     {
+for (int i = 0; i < nums.length; i++) {
+    for (int j = i+1; j < nums.length; j++){
+        if (nums[i] + nums [j]==target){
+            return new int[] {i,j};
+        }
+    }
+}
+return new int[] {};
+    }
+}
